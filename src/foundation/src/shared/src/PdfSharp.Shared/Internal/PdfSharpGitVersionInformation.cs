35cb01adea5d7a91ec70ab9aa5e0a438fcb9c1c9
Stepami <stepami@mail.ru>
2026-01-13 17:14:49 +0300

    Revert "comment gitversion.msbuild"
    This reverts commit e279819f

--- src/foundation/src/shared/src/PdfSharp.Shared/Internal/PdfSharpGitVersionInformation.cs
+++ src/foundation/src/shared/src/PdfSharp.Shared/Internal/PdfSharpGitVersionInformation.cs
@@ -1,8 +1,6 @@
 ﻿// PDFsharp - A .NET library for processing PDF
 // See the LICENSE file in the solution root for more information.
 
-using System.Globalization;
-
 namespace PdfSharp.Internal
 {
     /// <summary>
@@ -13,46 +11,46 @@ namespace PdfSharp.Internal
         /// <summary>
         /// The major version number of the product.
         /// </summary>
-        public static string Major = "1";//global::GitVersionInformation.Major;
+        public static string Major = global::GitVersionInformation.Major;
 
         /// <summary>
         /// The minor version number of the product.
         /// </summary>
-        public static string Minor = "1";//global::GitVersionInformation.Minor;
+        public static string Minor = global::GitVersionInformation.Minor;
 
         /// <summary>
         /// The patch number of the product.
         /// </summary>
-        public static string Patch = "1";//global::GitVersionInformation.Patch;
+        public static string Patch = global::GitVersionInformation.Patch;
 
         /// <summary>
         /// The Version pre-release string for NuGet.
         /// </summary>
-        public static string PreReleaseLabel = string.Empty;//global::GitVersionInformation.PreReleaseLabel;
+        public static string PreReleaseLabel =  global::GitVersionInformation.PreReleaseLabel;
 
         /// <summary>
         /// The full version number.
         /// </summary>
-        public static string MajorMinorPatch = "1.1.1";//global::GitVersionInformation.MajorMinorPatch;
+        public static string MajorMinorPatch = global::GitVersionInformation.MajorMinorPatch;
 
         /// <summary>
         /// The full semantic version number created by GitVersion.
         /// </summary>
-        public static string SemVer = "1.1.1";//global::GitVersionInformation.SemVer;
+        public static string SemVer = global::GitVersionInformation.SemVer;
 
         /// <summary>
         /// The full informational version number created by GitVersion.
         /// </summary>
-        public static string InformationalVersion = "1.1.1";//global::GitVersionInformation.InformationalVersion;
+        public static string InformationalVersion =  global::GitVersionInformation.InformationalVersion;
 
         /// <summary>
         /// The branch name of the product.
         /// </summary>
-        public static string BranchName = "refactoring/locks";//global::GitVersionInformation.BranchName;
+        public static string BranchName = global::GitVersionInformation.BranchName;
 
         /// <summary>
         /// The commit date of the product.
         /// </summary>
-        public static string CommitDate = DateTime.Today.ToString(CultureInfo.InvariantCulture);//global::GitVersionInformation.CommitDate;
+        public static string CommitDate = global::GitVersionInformation.CommitDate;
     }
 }
